a1302a51462fa9e1d06e621c579999b011af93d7
aerox0 <maerox1@gmail.com>
2022-07-19 15:41:30 +0300

    feat: simplify SimpleFileStorageBase

--- src/modules/storage/plaintext.ts
+++ src/modules/storage/plaintext.ts
@@ -8,8 +8,7 @@ export class SimpleFileStoragePlaintext extends SimpleFileStorageBase<string> {
 	 * @param  {string=''} data Passing data to constructor and then call save() will create a new file with data already inside it.
 	 */
 	constructor(file_path: string, data: string = '') {
-		super(file_path)
-		this.data = data
+		super(file_path, data)
 	}
 
 	async parseData(data_str: string): Promise<string> {
